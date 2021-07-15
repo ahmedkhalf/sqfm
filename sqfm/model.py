8a7f1b5548db2f3711f36ba70416db16d482ba60
ahmedkhalf <ahmedkhalf567@gmail.com>
2021-07-15 11:57:33 +0400

    Use QDir instead of pathlib

--- sqfm/model.py
+++ sqfm/model.py
@@ -1,46 +1,52 @@
-import typing
 from pathlib import Path
+from typing import List, Optional
 
-from PyQt5.QtCore import QAbstractTableModel, QModelIndex, QObject, Qt
+from PyQt5.QtCore import QAbstractTableModel, QDir, QModelIndex, QObject, Qt, QFileInfo
 
 
 class FileManagerModel(QAbstractTableModel):
-    def __init__(self, parent: typing.Optional[QObject] = None) -> None:
+    def __init__(self, parent: Optional[QObject] = None) -> None:
         super().__init__(parent=parent)
 
-        self.current_pwd = Path.home()
-        self.show_hidden = False
-        self.files = []
-
+        self.file_browser = FileBrowser()
         self.get_files()
 
     def get_files(self):
         self.layoutAboutToBeChanged.emit()
-        if self.show_hidden:
-            self.files = list(self.current_pwd.glob("*"))
-        else:
-            self.files = []
-            for file in self.current_pwd.glob("*"):
-                if file.name[0] != ".":
-                    self.files.append(file)
+        self.file_browser.list_files()
         self.layoutChanged.emit()
 
     def open_item(self, ind: int):
-        if self.files[ind].is_dir():
-            self.current_pwd = self.files[ind]
+        item = self.file_browser.files[ind]
+        if item.isDir():
+            print("isDir")
+            self.file_browser.current_pwd = QDir(item.filePath())
             self.get_files()
 
     def go_back(self):
-        self.current_pwd = self.current_pwd.parent
-        self.get_files()
+        if self.file_browser.current_pwd.cdUp():
+            self.get_files()
 
     def rowCount(self, ind: QModelIndex):
-        return len(self.files)
+        return len(self.file_browser.files)
 
     def columnCount(self, ind: QModelIndex):
         return 1
 
     def data(self, ind: QModelIndex, role: Qt.ItemDataRole):
         if role == Qt.DisplayRole:
-            return self.files[ind.row()].name
+            return self.file_browser.files[ind.row()].fileName()
         return None
+
+
+class FileBrowser:
+    def __init__(self) -> None:
+        self.current_pwd: QDir = QDir.home()
+
+        self.files: List[QFileInfo] = []
+
+    def list_files(self):
+        self.files = self.current_pwd.entryInfoList()
+
+    def refresh(self):
+        pass
